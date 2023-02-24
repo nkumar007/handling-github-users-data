ffdde82ff2e6bb2c66da48d1de06718de5fcad59
nkumar007 <naveen.mbuff98@gmail.com>
2023-02-24 15:36:38 +0530

    creating our components and setting up our Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
+import { useState } from "react";
+// react-router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//firebase
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ3TgDnqd9HRbRBZX1pA4Pqrn4uwO65b4",
+
+  authDomain: "myhub-2290c.firebaseapp.com",
+
+  projectId: "myhub-2290c",
+
+  storageBucket: "myhub-2290c.appspot.com",
+
+  messagingSenderId: "134964762185",
+
+  appId: "1:134964762185:web:896855772a1413a1619198",
+
+  measurementId: "G-YKK9R218TG",
+};
+
+const app = initializeApp(firebaseConfig);
+
+//Components
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import PageNotFound from "./pages/PageNotFound";
+import { UserContext } from "./context/UserContext";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+
 function App() {
-  return <div className="App">Made some changes</div>;
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signIn" component={SignIn} />
+          <Route exact path="/signUp" component={SignUp} />
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useContext } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+
+import { Link } from "react-router-dom";
+
+import { UserContext } from "../context/UserContext";
+
+const Header = () => {
+  const context = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <Navbar
+      color="info"
+      light
+      expand="md"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <NavbarBrand>
+        <Link to="/" className="text-white">
+          Firebase Github
+        </Link>
+      </NavbarBrand>
+      <NavbarText className="text-white">
+        {context.user?.email ? context.user.email : ""}
+      </NavbarText>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          {context.user ? (
+            <NavItem>
+              <NavLink tag={Link} to="/" className="text-white">
+                Logout
+              </NavLink>
+            </NavItem>
+          ) : (
+            <>
+              <NavItem>
+                <NavLink tag={Link} to="/" className="text-white">
+                  Signup
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/" className="text-white">
+                  Signin
+                </NavLink>
+              </NavItem>
+            </>
+          )}
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
